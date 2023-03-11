ca0d92d42f3e508b4850c90c9dfb932501873b2a
Kris Magat <kmag.dev@gmail.com>
2023-03-10 22:27:55 -0800

    completed all endpoints

--- src/controllers/db-controller.ts
+++ src/controllers/db-controller.ts
@@ -3,57 +3,60 @@ import { NoteModel } from '../db';
 
 interface Note { id: number ; date: string; note: string };
 
+const dbController = {
+  getNotes: async (req: Request, res: Response, next: any) => {
+    try {
+      const query = await NoteModel.find();
+      res.locals.data = query;
+      console.log(query);
+      return next();
+    }
+    catch (err) {
+      const error = {message: 'Error in getNotes controller.', error: err}
+      next(error);
+    }
+  },
 
-const getNotes = async (req: Request, res: Response, next: any) => {
-  try {
-    const query = await NoteModel.find();
-    res.locals.data = query;
-    console.log(query);
-    return next();
-  }
-  catch (err) {
-    const error = {message: 'Error in getNotes controller.', error: err}
-    next(error);
-  }
-};
+  addNote: async (req: Request, res: Response, next: any) => {
+    const {date, note} = req.body;
+    try {
+      const query = await NoteModel.create({date, note});
+      res.locals.data = query;
+      console.log(query);
+      return next();
+    }
+    catch (err) {
+      const error = {message: 'Error in addNote controller.', error: err}
+      next(error);
+    }
+  },
 
-const addNote = async (req: Request, res: Response, next: any) => {
-  const {date, note} = req.body;
-  try {
-    const query = await NoteModel.create({date, note});
-    res.locals.data = query;
-    console.log(query);
-    return next();
-  }
-  catch (err) {
-    const error = {message: 'Error in addNote controller.', error: err}
-    next(error);
-  }
-};
+  editNote: async (req: Request, res: Response, next: any) => {
+    const { _id, date, note } = req.body;
+    try {
+      const query = await NoteModel.findByIdAndUpdate(_id, { date, note });
+      res.locals.data = query;
+      return next();
+    }
+    catch (err) {
+      const error = {message: 'Error in editNote controller.', error: err}
+      next(error);
+    }
+  },
 
-const editNote = (req: Request, res: Response, next: any) => {
-  try {
-    //findByIdAndUpdate()
-    res.send('Success!');
-  }
-  catch (err) {
-    const error = {message: 'Error in editNote controller.', error: err}
-    next(error);
-  }
-};
+  deleteNote: async (req: Request, res: Response, next: any) => {
+    const id = req.body._id;
+    try {
+      const query = await NoteModel.findByIdAndRemove(id);
+      res.locals.data = query;
+      return next();
+    }
+    catch (err) {
+      const error = {message: 'Error in deleteNote controller.', error: err}
+      next(error);
+    }
+  },
+}
 
-const deleteNote = (req: Request, res: Response, next: any) => {
-  try {
-    //findByIdAndRemove()
-    res.send('Success!');
-  }
-  catch (err) {
-    const error = {message: 'Error in deleteNote controller.', error: err}
-    next(error);
-  }
-};
-
-
-
-export { getNotes, addNote };
+export const { getNotes, addNote, editNote, deleteNote } = dbController;
 
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,22 +1,10 @@
-import express, { Request, Response } from 'express';
-import { getNotes, addNote } from '../controllers/db-controller'
+import express from 'express';
+import { getNotes, addNote, editNote, deleteNote } from '../controllers/db-controller';
 const router = express.Router();
 
-const middleware = (req: Request, res: Response, next: any) => {
-  try {
-    res.send('Success!');
-  }
-  catch (err) {
-    next(err);
-  }
-};
-
 router.get('/', getNotes, (req, res) => res.status(200).send(res.locals.data));
-
 router.post('/', addNote, (req, res) => res.status(200).send(res.locals.data));
-
-router.put('/', middleware);
-
-router.delete('/', middleware);
+router.put('/', editNote, (req, res) => res.status(200).send(res.locals.data));
+router.delete('/', deleteNote, (req, res) => res.status(200).send(res.locals.data));
 
 export { router as apiRouter };
